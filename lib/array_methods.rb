95c5f825bbebe71940c64d79d08ae27cd0733255
David Holmes <phoenix1988@gmail.com>
2019-12-18 16:10:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include?${element}
+  array.include?(element) 
 end
 
 def using_sort(array)
@@ -25,5 +25,5 @@ end
 
 def using_size(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.size
+  array.size 
 end
